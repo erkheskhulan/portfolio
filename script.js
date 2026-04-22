700fd53e454a69a73fd194cf863bbaf09a401f4a
erkheskhulan <khulan20080730@gmail.com>
2026-04-22 21:15:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function showMusic(mood){
+
+let music = document.getElementById("music");
+
+if(mood === "study"){
+music.innerHTML =
+'<iframe width="400" height="220" src="https://www.youtube.com/embed/jfKfPfyJRdk"></iframe>';
+}
+
+if(mood === "sad"){
+music.innerHTML =
+'<iframe width="400" height="220" src="https://www.youtube.com/embed/4fndeDfaWCg"></iframe>';
+}
+
+if(mood === "happy"){
+music.innerHTML =
+'<iframe width="400" height="220" src="https://www.youtube.com/embed/ZbZSe6N_BXs"></iframe>';
+}
+
+if(mood === "tired"){
+music.innerHTML =
+'<iframe width="400" height="220" src="https://www.youtube.com/embed/5qap5aO4i9A"></iframe>';
+}
+
+}
